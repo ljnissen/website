9c4cec2f23ec9d1301aed8e817102b7e91b5d73f
ljnissen <lj_nissen@gmx.de>
2015-01-04 21:56:53 +0100

    Fixed the last PHP (for contact)

---
+++ ljnissen dot com/acknowledge.php
@@ -0,0 +1,45 @@
+<?php
+	if (isset($_POST['send'])) {
+		$to = 'lj.nissen@googlemail.com';
+		$subject = 'From website';
+		$message = 'Name: ' . $_POST['name'] . "\r\n\r\n";
+		$message .= 'Email: ' . $_POST['email'] . "\r\n\r\n";
+		$message .= 'Message: ' . $_POST['message'];
+		$headers = "From: webmaster@coquilles-web-logo.se\r\n";
+		$headers .= 'Content-Type: text/plain; charset=utf-8';
+	}
+		$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+		if ($email) {
+			$headers .= "\r\nReply-To: $email";
+		}
+
+		$success = mail($to, $subject, $message, $headers, '-flj.nissen@gmail.com');
+?>
+
+
+
+<!doctype>
+<html>
+
+<head>
+	<link rel="stylesheet" type="text/css" href="./css/contact.css" />
+</head>
+
+<link rel="stylesheet" href="./css/contact.css" />
+<meta charset="utf-8">
+
+<style type="text/css">
+</style>
+
+<body>
+
+<?php if (isset($success) && $success) { ?>	
+<h1>Thank you!</h1>
+Your message has been sent.
+<?php } else { ?>
+<h1>Sorry</h1>
+There was a problem sending your message.
+<?php } ?>
+
+</body>
+</html>
